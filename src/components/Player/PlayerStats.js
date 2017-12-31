ccc010c77318ff33b5965f860a3d41d97abd8de8
hcam05 <hcam05@gmail.com>
2017-12-31 14:53:55 -0800

    changed the tab space settings

--- src/components/Player/PlayerStats.js
+++ src/components/Player/PlayerStats.js
@@ -1,51 +1,50 @@
 import React from 'react';
 import {
-    Table,
-    TableBody,
-    TableHeader,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn,
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
 } from 'material-ui/Table';
 
 class PlayerStats extends React.Component {
 
-    render() {
-        const playerStats = this.props.data
-        // console.log(this.props)
-        const playerList = []
-        if (this.props.data.length !== 0) {
-            for (let i = this.props.start; i <= this.props.end; i++) {
-                playerList.push(
-                    <TableRow key={this.props.data[i].id}>
-                        <TableRowColumn>{this.props.data[i].name}</TableRowColumn>
-                        <TableRowColumn>{this.props.data[i].position}</TableRowColumn>
-                        <TableRowColumn>{this.props.data[i].team}</TableRowColumn>
-                        <TableRowColumn>{this.props.data[i].seasonPts}</TableRowColumn>
-                        <TableRowColumn>{this.props.data[i].weekPts}%</TableRowColumn>
-                    </TableRow>
-                )
-            }
-        }
-        return (
-            <div>
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHeaderColumn>Name</TableHeaderColumn>
-                            <TableHeaderColumn>Positon</TableHeaderColumn>
-                            <TableHeaderColumn>Team</TableHeaderColumn>
-                            <TableHeaderColumn>Season Points</TableHeaderColumn>
-                            <TableHeaderColumn>Week Points</TableHeaderColumn>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {playerList}
-                    </TableBody>
-                </Table>
-            </div>
+  render() {
+    const playerStats = this.props.data
+    const playerList = []
+    if (this.props.data.length !== 0) {
+      for (let i = this.props.start; i <= this.props.end; i++) {
+        playerList.push(
+          <TableRow key={this.props.data[i].id}>
+            <TableRowColumn>{this.props.data[i].name}</TableRowColumn>
+            <TableRowColumn>{this.props.data[i].position}</TableRowColumn>
+            <TableRowColumn>{this.props.data[i].team}</TableRowColumn>
+            <TableRowColumn>{this.props.data[i].seasonPts}</TableRowColumn>
+            <TableRowColumn>{this.props.data[i].weekPts}%</TableRowColumn>
+          </TableRow>
         )
+      }
     }
+    return (
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Positon</TableHeaderColumn>
+              <TableHeaderColumn>Team</TableHeaderColumn>
+              <TableHeaderColumn>Season Points</TableHeaderColumn>
+              <TableHeaderColumn>Week Points</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {playerList}
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
 }
 
 export default PlayerStats;
--- src/components/Player/PlayerTable.js
+++ src/components/Player/PlayerTable.js
@@ -11,109 +11,66 @@ import { green800, green500, grey800 } from 'material-ui/styles/colors';
 import { Table } from 'material-ui/Table'
 import { TableBody } from 'material-ui/Table/TableBody';
 
-
 // GraphQL set up with react-apollo library
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-// DATABASE CONNECTION
-// const mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/nfl_api_data');
-// mongoose.connection.once('open', () => {
-//   console.log('Connected to Database');
-// });
-// import Player from '../../model/playerModel';
-
 const muiTheme = getMuiTheme({
-    palette: {
-        textColor: grey800,
-    },
-    appBar: {
-        height: 50,
-    },
+  palette: {
+    textColor: grey800,
+  },
+  appBar: {
+    height: 50,
+  },
 
 });
 
 class PlayerTable extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            players: [],
-            start: 0,
-            end: 9,
-            year: 2017,
-            week: 15,
-        };
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+      start: 0,
+      end: 9,
+      year: 2017,
+      week: 15,
     };
+  };
 
-    nextPg() {
-        if (this.state.end < 99) {
-            this.setState({
-                start: this.state.start + 10,
-                end: this.state.end + 10
-            });
-        }
+  nextPg() {
+    if (this.state.end < 99) {
+      this.setState({
+        start: this.state.start + 10,
+        end: this.state.end + 10
+      });
     }
+  }
 
-    prevPg() {
-        if (this.state.start > 0) {
-            this.setState({
-                start: this.state.start - 10,
-                end: this.state.end - 10
-            });
-        }
+  prevPg() {
+    if (this.state.start > 0) {
+      this.setState({
+        start: this.state.start - 10,
+        end: this.state.end - 10
+      });
     }
+  }
 
-    // getData() {
-    //     //connect to mongoDB
-    //     //query for default year and default week
-    //     //set state
-    //     console.log('v2 fired');
-    //     // return new Promise((resolve, reject) => {
-    //     //     Player.find({ 'season': 2017, 'week': 16 })
-    //     //         .sort({ 'weekPts': -1 })
-    //     //         .exec()
-    //     //         .then(data => {
-    //     //             this.setState(({players: data}))
-    //     //         })
-    //     //         .catch(err => reject(err));
-    //     // })
+  render() {
+    console.log(this.props);
 
-    //     // axios.get(url).then(res => res.data.map(x => {
-    //     //     this.setState({ crypto: res.data });
-    //     // }));
-    // }
+    return (
+      <div>
+        <AppBar title="FFApp" style={{ backgroundColor: green800 }}>
+        </AppBar>
+        <br />
+        {(this.props.data.loading) ? <div>Loading</div> : <PlayerStats data={this.props.data.week} start={this.state.start} end={this.state.end} />}
+        <br />
+        <RaisedButton onClick={() => this.prevPg()}>Prev</RaisedButton>
+        <RaisedButton onClick={() => this.nextPg()}>Next</RaisedButton>
+      </div>
 
-    // filter() {
-
-    // }
-
-    componentWillMount() {
-        // if (!this.props.data.loading) {
-        //     console.log('set state');
-        //     this.setState({players: this.props.data.week});
-        // }
-    }
-
-
-
-    render() {
-        console.log(this.props);
-        
-        return (
-            <div>
-                <AppBar title="FFApp" style={{ backgroundColor: green800 }}>
-                </AppBar>
-                <br />
-                {(this.props.data.loading) ? <div>Loading</div> : <PlayerStats data={this.props.data.week} start={this.state.start} end={this.state.end} /> }
-                <br />
-                <RaisedButton onClick={() => this.prevPg()}>Prev</RaisedButton>
-                <RaisedButton onClick={() => this.nextPg()}>Next</RaisedButton>
-            </div>
-
-        );
-    }
+    );
+  }
 }
 //GRAPHQL QUERY
 const query = gql`
