d4493489f527a01bb347f37f45ff32df975f7199
hcam05 <hcam05@gmail.com>
2017-12-31 14:29:08 -0800

    added apollo dependecies for graphQL queries

--- src/app.js
+++ src/app.js
@@ -7,6 +7,18 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { grey800 } from 'material-ui/styles/colors';
 
+//Apollo Client Depenedcies//
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: 'http://localhost:9000/graphql' }),
+    cache: new InMemoryCache()
+});
+
+
 const muiTheme = getMuiTheme({
     palette: {
         textColor: grey800,
@@ -18,8 +30,10 @@ const muiTheme = getMuiTheme({
 });
 
 ReactDOM.render(
-    <MuiThemeProvider muiTheme={muiTheme}>
-        <PlayerTable />
-    </MuiThemeProvider>
-    , 
+    <ApolloProvider client={client} >
+        <MuiThemeProvider muiTheme={muiTheme}>
+            <PlayerTable />
+        </MuiThemeProvider>
+    </ ApolloProvider>
+    ,
     document.getElementById('mount'));
