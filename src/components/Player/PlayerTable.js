76b7b7845159977d08282f11cd477e29065cef9c
hcam05 <hcam05@gmail.com>
2018-01-12 11:43:23 -0800

    stripping out material ui

--- src/components/Player/PlayerTable.js
+++ src/components/Player/PlayerTable.js
@@ -2,29 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import PlayerStats from './PlayerStats';
 
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
-import { green800, green500, grey800 } from 'material-ui/styles/colors';
-import { Table } from 'material-ui/Table'
-import { TableBody } from 'material-ui/Table/TableBody';
-
 // GraphQL set up with react-apollo library
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-
-const muiTheme = getMuiTheme({
-  palette: {
-    textColor: grey800,
-  },
-  appBar: {
-    height: 50,
-  },
-
-});
-
 class PlayerTable extends React.Component {
   constructor() {
     super();
@@ -51,7 +31,7 @@ class PlayerTable extends React.Component {
         end: this.state.end + 50
       });
     }
-  }
+  };
 
   prevPg() {
     if (this.state.start > 0) {
@@ -60,7 +40,7 @@ class PlayerTable extends React.Component {
         end: this.state.end - 50,
       });
     }
-  }
+  };
 
   render() {
     console.log(this.props.weekStats);
@@ -71,13 +51,12 @@ class PlayerTable extends React.Component {
 
     return (
       <div>
-        <AppBar title="Fantasy Football Dashboard " style={{ backgroundColor: green800 }}>
-        </AppBar>
+        <div>Fantasy Football Dashboard</div>
         <br />
-        {(this.props.weekStats.week) ? <PlayerStats data={this.props.weekStats.week} start={this.state.start} end={this.state.end} key={this.props.weekStats.week[0].week}/> : <div>Loading</div>}
-        <br />
-        <RaisedButton onClick={() => this.prevPg()}>Prev</RaisedButton>
-        <RaisedButton onClick={() => this.nextPg()}>Next</RaisedButton>
+          {(this.props.weekStats.week) ? <PlayerStats data={this.props.weekStats.week} start={this.state.start} end={this.state.end} key={this.props.weekStats.week[0].week} /> : <div>Loading</div>}
+          <br />
+        <button onClick={() => this.prevPg()}>Prev</button>
+        <button onClick={() => this.nextPg()}>Next</button>
       </div>
 
     );
@@ -94,6 +73,7 @@ week(week: 16, season: 2017){
     seasonPts
     week
     season
+    id
     }
 }
 `;
