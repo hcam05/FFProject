434bc91cc2cf9576f12d36f15fdc2ddece5fce76
hcam05 <hcam05@gmail.com>
2018-01-12 16:41:41 -0800

    adding player control component

---
+++ src/components/Player/PlayerControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PlayerControl = (props) => {
+
+  return (
+    <div>
+      Filter Position
+      <button onClick={this.selectQB}>QB</button>
+      <button onClick={this.selectRB}>RB</button>
+      <button onClick={this.selectWR}>WR</button>
+      <button onClick={this.selectTE}>TE</button>
+      <button onClick={this.selectDEF}>DEF</button>
+      <button onClick={this.selectK}>K</button>
+      <button onClick={this.selectK}>LB</button>
+      <button onClick={this.selectK}>DB</button>
+      <button onClick={this.selectK}>DL</button>
+    </div>
+  )
+
+
+}
