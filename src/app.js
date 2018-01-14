08f1903c253e5221b448148aa5982adb9e8bdc4f
hcam05 <hcam05@gmail.com>
2018-01-14 10:46:19 -0800

    stripped out graphql dependencies

--- src/app.js
+++ src/app.js
@@ -1,39 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PlayerTable from './components/Player/PlayerTable';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { grey800 } from 'material-ui/styles/colors';
-
-//Apollo Client Depenedcies//
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://localhost:9000/graphql' }),
-    cache: new InMemoryCache()
-});
-
-
-const muiTheme = getMuiTheme({
-    palette: {
-        textColor: grey800,
-    },
-    appBar: {
-        height: 50,
-    },
-
-});
-
-ReactDOM.render(
-    <ApolloProvider client={client} >
-        <MuiThemeProvider muiTheme={muiTheme}>
-            <PlayerTable />
-        </MuiThemeProvider>
-    </ ApolloProvider>
-    ,
-    document.getElementById('mount'));
+ReactDOM.render(<PlayerTable />, document.getElementById('mount'));
